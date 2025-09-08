d7ada95b92c9e6f4a98209b94024c808cda9a9e3
EskilE <eskilme08@gmail.com>
2025-09-08 11:59:22 +0200

    Small change with use of single and double quotes

--- Chatbot/backend.py
+++ Chatbot/backend.py
@@ -3,15 +3,15 @@ import ai_logic as ai
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def chat():
-    userInput = request.form['userInput']
+    userInput = request.form["userInput"]
     AIOutput = ai.get_ai_response(userInput)
     return render_template("index.html", aiOutput=AIOutput)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
