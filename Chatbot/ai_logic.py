e348580cf1dc59b2651852321956ac56411962c3
EskilE <eskilme08@gmail.com>
2025-09-09 13:33:31 +0200

    change abit with the apikey

--- Chatbot/ai_logic.py
+++ Chatbot/ai_logic.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
 from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import SystemMessage, UserMessage, AssistantMessage
 from azure.core.credentials import AzureKeyCredential
 
+load_dotenv()
+
 client = ChatCompletionsClient(
     endpoint="https://models.github.ai/inference",
-    credential=AzureKeyCredential(os.environ["GITHUB_TOKEN"]),
+    credential=AzureKeyCredential(os.environ["ApiKey"]),
     api_version="2024-12-01-preview",
 )
 
